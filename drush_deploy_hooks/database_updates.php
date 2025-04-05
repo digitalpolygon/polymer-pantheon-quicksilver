2ff9eeeec5be2d2fca88326a6e8bcb8a9a99cad5
DDEV User <nobody@example.com>
2025-04-04 23:56:42 -0400

    Initial commit.

---
+++ drush_deploy_hooks/database_updates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Runs `$ drush updb` command.
+ */
+
+ echo "Running Drupal DB updates...\n";
+ $drush_command = 'drush updb --yes --no-interaction 2>&1';
+ echo "Executing Drush command: $drush_command\n";
+ passthru($drush_command, $exit_code);
+ if ($exit_code > 0) {
+   echo "Drush command failed with exit code: $exit_code.\n";
+ }
+ else {
+   echo "Drush command executed successfully.\n";
+ }
